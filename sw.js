7c07fe000959f42a2e87f8d4c93060764b5441c3
Angel Ramses <146279784+MasterCode27@users.noreply.github.com>
2026-09-08 09:14:59 -0600

    Mejoras nuevo panel de notificaciones

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE="guardias-v16.0";
+const CACHE="guardias-v16.2";
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"])).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener("notificationclick",e=>{e.notification.close();e.waitUntil(clients.matchAll({type:"window",includeUncontrolled:true}).then(list=>{for(const c of list){if("focus"in c)return c.focus()}if(clients.openWindow)return clients.openWindow("./")}))});
